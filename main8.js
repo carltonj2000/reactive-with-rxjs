0469d7f60ce1737b191f320f754d643f58b95bca
Carlton Joseph <carlton.joseph@gmail.com>
2018-07-09 18:30:27 -0700

    add code to refresh a single user

--- main8.js
+++ main8.js
@@ -1,25 +1,10 @@
 const { of, from, fromEvent } = rxjs;
-const {
-  tap,
-  map,
-  flatMap,
-  merge,
-  startWith,
-  shareReplay,
-  withLatestFrom
-} = rxjs.operators;
+const { tap, map, flatMap, merge, startWith, shareReplay } = rxjs.operators;
 
 const url = `https://api.github.com/users?client_id=${clientId}&client_secret=${clientSecret}`;
 console.group("video 6");
 
 (() => {
-  const close1 = document.querySelector(".close1");
-  const close2 = document.querySelector(".close2");
-  const close3 = document.querySelector(".close3");
-  const close1click = fromEvent(close1, "click");
-  const close2click = fromEvent(close2, "click");
-  const close3click = fromEvent(close3, "click");
-
   const startup = of(url);
   const refresh = document.querySelector(".refresh");
   const refClick = fromEvent(refresh, "click");
@@ -30,29 +15,22 @@ console.group("video 6");
       return of(`${url}&since=${rmndOfst}`);
     })
   );
-  const reqStream = reqRefeshStream.pipe(merge(startup));
-  const respStream = reqStream.pipe(
+  const respStream = reqRefeshStream.pipe(
+    merge(startup),
     flatMap(u => from(fetch(url)).pipe(flatMap(resp => resp.json()))),
     shareReplay(1)
   );
-  const getRandomUser = listUsers =>
-    listUsers[Math.floor(Math.random() * listUsers.length)];
-  const createSuggestionStream = (responseStream, closeClick) => {
+  const createSuggestionStream = responseStream => {
     return responseStream.pipe(
-      map(listUsers => getRandomUser(listUsers)),
+      map(listUsers => listUsers[Math.floor(Math.random() * listUsers.length)]),
       startWith(null),
-      merge(reqRefeshStream.pipe(map(e => null))),
-      merge(
-        closeClick.pipe(
-          withLatestFrom(respStream, (ev, us) => getRandomUser(us))
-        )
-      )
+      merge(reqRefeshStream.pipe(map(e => null)))
     );
   };
 
-  const suggestionStream1 = createSuggestionStream(respStream, close1click);
-  const suggestionStream2 = createSuggestionStream(respStream, close2click);
-  const suggestionStream3 = createSuggestionStream(respStream, close3click);
+  const suggestionStream1 = createSuggestionStream(respStream);
+  const suggestionStream2 = createSuggestionStream(respStream);
+  const suggestionStream3 = createSuggestionStream(respStream);
 
   const renderSuggestion = (user, selector) => {
     const elem = document.querySelector(selector);
